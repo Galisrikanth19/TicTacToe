d4d4c4c67c7e8caa1bf4f3487134cde10f3ef6e4
Gali Srikanth <GaliSrikanth@gali-srikanth-mba.local>
2018-09-20 08:30:08 +0530

    Few issues fixed

---
+++ TicTacToe/MVC/Model/ModelClass.swift
@@ -0,0 +1,13 @@
+//
+//  ModelClass.swift
+//  HelloWorld
+//
+//  Created by Gali Srikanth on 20/09/18.
+//  Copyright © 2018 Gs19. All rights reserved.
+//
+
+import UIKit
+
+class ModelClass: NSObject {
+
+}
